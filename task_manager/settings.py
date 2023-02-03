92dd27c36fca606a2d7ec03c8ff72e027d6cae82
Chouaib-Djerdi <djerdichouaib@gmail.com>
2023-02-03 18:53:50 +0100

    security commit

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b%1kazsczr%eg^hl8m^s%8xk9#m$mm322pst*%-$&p3k=fhw1p'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
